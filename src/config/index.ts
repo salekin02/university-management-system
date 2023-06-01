5dbb14b1ddd89ebd6e9b720be92575dd4e89cc07
xs-salekin <36966165+nahidsalekin@users.noreply.github.com>
2023-06-01 17:22:09 +0600

    ...

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-import path from 'path';
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port: process.env.PORT || 3000,
-    database_url: process.env.DATABASE_URL || 'mongodb://localhost:27017/test',
+  port: process.env.PORT || 3000,
+  database_url: process.env.DATABASE_URL || 'mongodb://localhost:27017/test',
 }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("Connected to MongoDB");
-        app.listen(config.port, () => {
-            console.log(`Example app listening at http://localhost:${config.port}`);
-            }
-        );
-    }
-    catch (err) {
-        console.error(err);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Connected to MongoDB')
+    app.listen(config.port, () => {
+      console.log(`Example app listening at http://localhost:${config.port}`)
+    })
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-bootstrap();
+bootstrap()
