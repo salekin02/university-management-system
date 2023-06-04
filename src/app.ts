ed22a183068bc9bce0886054a1372ee5b917cc3a
Nahid Salekin <36966165+nahidsalekin@users.noreply.github.com>
2023-06-04 22:04:40 +0600

    auth updated

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from 'express'
+import usersService from './modules/users/users.service';
+import userRouter from './modules/users/users.route';
 const app: Application = express()
 import cors from 'cors'
 
@@ -7,7 +9,16 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+//application routes
+
+app.use('/api/v1/users',userRouter)
+
+app.get('/', async(req: Request, res: Response) => {
+  await usersService.createUser({
+    id: '1',
+    password: '1234',
+    role: 'admin',
+  });
   res.send('Hello World!')
 })
 
