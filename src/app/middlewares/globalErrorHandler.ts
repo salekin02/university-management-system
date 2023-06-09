bc8609453f99ddb9e4e814c9ebc4aaab797ba3e8
Nahid Salekin <36966165+nahidsalekin@users.noreply.github.com>
2023-06-09 16:12:38 +0600

    error handle basic setup

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,31 +1,41 @@
 import { NextFunction, Request, Response } from 'express'
 import config from '../../config'
 import { IGenericErrorMessage } from '../../interfaces/error'
+import { handleValidationError } from '../../errors/handleValidationError'
+import ApiError from '../../errors/ApiError'
 
 const globalErrorHandler = (
-  err: any,
+  err,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  // if(err instanceof Error){
-  //   res.status(400).json({error:err})
-  // }
-  // else{
-  //   res.status(500).json({error: 'something went wrong'})
-  // }
-  next(err)
+  let statusCode = 500
+  let message = 'Something went wrong'
+  let errorMessages: IGenericErrorMessage[] = []
 
-  const statusCode = 500
-  const message = 'Something went wrong'
-  const errorMessages: IGenericErrorMessage[] = []
-
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } 
+  else if(err instanceof ApiError){
+    statusCode = err.statusCode
+    message = err.message
+    errorMessages = err.message ? [{ path: '', message: err?.message }] : []
+  }
+  else if (err instanceof Error) {
+    message = err?.message
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : []
+  }
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
     stack: config.env !== 'production' ? err?.stack : undefined,
   })
+  next()
 }
 
 export default globalErrorHandler
