b8592fb74bb0d67f5a78f0cd7706003c6f4b856b
xs-salekin <36966165+nahidsalekin@users.noreply.github.com>
2023-06-12 09:37:55 +0600

    .

--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,19 +1,20 @@
-import mongoose from "mongoose";
-import { IGenericErrorMessage } from "../interfaces/error";
-import { IGenericErrorResponse } from "../interfaces/common";
+import mongoose from 'mongoose'
+import { IGenericErrorMessage } from '../interfaces/error'
+import { IGenericErrorResponse } from '../interfaces/common'
 
-export const handleValidationError = (err: mongoose.Error.ValidationError): IGenericErrorResponse => {
-    const errors: IGenericErrorMessage[] = Object.values(err.errors).map(
-        (el) => {
-        return {
-            message: el?.message,
-            path: el?.path,
-        }
-    })
-    const statusCode = 400
+export const handleValidationError = (
+  err: mongoose.Error.ValidationError
+): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = Object.values(err.errors).map(el => {
     return {
-        statusCode,
-        message: 'Validation Error',
-        errorMessages: errors
+      message: el?.message,
+      path: el?.path,
     }
+  })
+  const statusCode = 400
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorMessages: errors,
+  }
 }
