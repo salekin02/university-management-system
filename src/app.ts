37286574b9c07355473b738f7a6e54ea41fe566c
salekin02 <36966165+salekin02@users.noreply.github.com>
2024-11-26 16:44:51 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const app: Application = express()
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import routes from './app/routes'
-import { generateFacultyId, generateStudentId } from './app/modules/user/user.utils'
+import { generateStudentId } from './app/modules/user/user.utils'
 
 app.use(cors())
 
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //application routes
+
 app.use('/api/v1/', routes)
 
 //global error handler
@@ -31,7 +32,7 @@ app.use((req, res, next) => {
     year: '2021',
   }
 
-  const testId = await generateFacultyId()
+  const testId = await generateStudentId(academicSemester)
   console.log(testId)
 })()
 
